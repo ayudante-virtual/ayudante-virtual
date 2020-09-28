ad0321a6b0c65afad9e25ca8a35543deb7100ce0
seba <sebastian.blazquez96@gmail.com>
2020-09-28 11:47:02 -0300

    Permito enviar imágenes en la consulta

--- data/global/actions/ayudante-virtual/ensamblarConsulta.js
+++ data/global/actions/ayudante-virtual/ensamblarConsulta.js
@@ -1,3 +1,9 @@
+const adjuntos = (archivos) => {
+    return archivos ? "\nCon los archivos:\n" +
+        archivos.map(archivo => ` • <${archivo.permalink}|${archivo.name}>`)
+            .join("\n") : '';
+}
+
 /**
  * Ensambla la consulta para que incluya los archivos adjuntos.
  * @title Ensamblar consulta
@@ -5,17 +11,10 @@
  * @author Seblaz.
  */
 const ensamblarConsulta = async () => {
-    // console.log(event.payload && event.payload.payload && event.payload.payload.files)
-    event.state.temp.consulta = event.preview //+
-        // adjuntos(event.payload.payload && event.payload.payload.files)
-    // console.log(event.payload.payload.files)
-    // console.log(event.state.temp.consulta)
-}
-
-const adjuntos = (archivos) => {
-    return archivos ? "\nCon los archivos:\n" +
-        archivos.map(archivo => ` • <${archivo.permalink}|${archivo.name}>`)
-            .join("\n") : '';
+    let extra = ''
+    if(event.payload.payload && event.payload.payload.files)
+        extra = adjuntos(event.payload.payload.files)
+    event.state.temp.consulta = (event.preview || '') + extra
 }
 
 return ensamblarConsulta()
