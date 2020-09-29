42616a625a41ffd4e67a998c695840b8d9685c57
seba <sebastian.blazquez96@gmail.com>
2020-09-28 21:37:41 -0300

    Fix getCanalDeConsultas

--- data/global/actions/ayudante-virtual/getCanalDeConsultas.js
+++ data/global/actions/ayudante-virtual/getCanalDeConsultas.js
@@ -8,7 +8,7 @@
 const getCanalDeConsultas = async () => {
     const botData = await bp.kvs.forBot(event.botId).get('global')
     const teamId = event.payload.body.team.id
-    if(botData[teamId] && botData[teamId].canal_de_consultas) {
+    if(botData && botData[teamId] && botData[teamId].canal_de_consultas) {
         event.state.temp.canal_de_consultas = botData[teamId].canal_de_consultas
     } else {
         event.state.temp.canal_de_consultas = false
